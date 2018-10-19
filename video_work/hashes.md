d22053b149cde5eebe523f29fc11cdbfcf5b2dab
Jennifer G <jennifer.garza2691@gmail.com>
2018-10-19 00:28:43 +0000

    Thursdays, works

---
+++ video_work/hashes.md
@@ -0,0 +1,7 @@
+#array that describes Jennifer and her other online name 
+person = ['Jenny', 'Androssi','internet','reality']
+person ={'nick_name' => 'Jenny','enter_name' => 'androssi', 'www' => 'internet', 'air' =>reality}
+person ['nick_name']
+person [www]
+person.index ('Jenny')
+mixed = 
---
+++ video_work/level_two_notes.rb
@@ -0,0 +1,27 @@
+justice = ["superman", "batman" , "Aquaman"]
+justice [2]
+justice [1]= "Green Lantern"
+justice
+justice << "Wonder Woman" 
+justice [0] = nil
+justice
+
+marvel = ["Hulk", 1945 , 'cold war' , ["carter", "capt"] , "Beach boy"]
+marvel.inspect
+justice.inspect
+puts justice
+puts marvel.inspect
+puts marvel
+marvel.to_s
+marvel.join
+marvel.join(“,”)
+
+
+hawkeye = "Roy, Hudges, Armstrong, Mira, Kain, Maria"
+edward = hawkeye.split(',')
+puts edward
+edward.reverse
+marvel.delete(beach boy)
+
+#after this I just kept watching the vido and review notes from tuesday.
+#goal is to get past hashes by saturday and the practice.
